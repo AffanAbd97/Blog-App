e7deb83c7d80e643476b744bb8301f9f487772b6
Affan-97 <affana959@gmail.com>
2024-04-26 03:24:34 +0700

    update Readme

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>Dashboard</title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('src/css/font-awesome/all.min.css') }}">
@@ -37,11 +37,6 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
-        {{-- <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('src/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-                height="60" width="60">
-        </div> --}}
 
         <!-- Navbar -->
         <x-navbar />
